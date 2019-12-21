37a2c39f275a5208b16e16abe592e5e5c5d5a37f
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-21 05:41:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,7 +26,11 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  total = 0
+
   director_data[:movies].each do |movie|
     total += movie[worldwide_gross]
   end
+
+  total
 end
