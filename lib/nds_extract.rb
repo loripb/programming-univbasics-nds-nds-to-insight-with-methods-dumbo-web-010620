cc7bd9714da1ef379999def3acd2da1e5b858e26
Lori Boyd <lori.p.boyd@gmail.com>
2019-12-21 05:22:43 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -30,7 +30,7 @@ def gross_for_director(director_data)
   gross = 0
 
   while movie_index < director_data.length
-    print director_data[movie_index][:worldwide_gross] # stores worldwide_gross in a variable
+    gross =  director_data[movie_index][:worldwide_gross] # stores worldwide_gross in a variable
     movie_index += 1
   end
 
